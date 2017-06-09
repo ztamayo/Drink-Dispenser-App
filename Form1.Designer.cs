c227da770b48a4352762978e32523c1bf5e525df
Ztamayo <Zailyn@gmail.com>
2017-06-08 19:53:29 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Drink_Dispenser_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Dispensebutton = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.CreamcheckBox = new System.Windows.Forms.CheckBox();
+            this.SugarcheckBox = new System.Windows.Forms.CheckBox();
+            this.HoneycheckBox = new System.Windows.Forms.CheckBox();
+            this.PepsiradioButton = new System.Windows.Forms.RadioButton();
+            this.DietradioButton = new System.Windows.Forms.RadioButton();
+            this.SMradioButton = new System.Windows.Forms.RadioButton();
+            this.RTearadioButton = new System.Windows.Forms.RadioButton();
+            this.PTearadioButton = new System.Windows.Forms.RadioButton();
+            this.WaterradioButton = new System.Windows.Forms.RadioButton();
+            this.CoffeeradioButton = new System.Windows.Forms.RadioButton();
+            this.TearadioButton = new System.Windows.Forms.RadioButton();
+            this.HCradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Beverage Dispenser";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.WaterradioButton);
+            this.groupBox1.Controls.Add(this.PTearadioButton);
+            this.groupBox1.Controls.Add(this.RTearadioButton);
+            this.groupBox1.Controls.Add(this.SMradioButton);
+            this.groupBox1.Controls.Add(this.DietradioButton);
+            this.groupBox1.Controls.Add(this.PepsiradioButton);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(411, 147);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cold Drinks";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.HCradioButton);
+            this.groupBox2.Controls.Add(this.TearadioButton);
+            this.groupBox2.Controls.Add(this.CoffeeradioButton);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 268);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 142);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hot Drinks";
+            // 
+            // Dispensebutton
+            // 
+            this.Dispensebutton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dispensebutton.Location = new System.Drawing.Point(32, 434);
+            this.Dispensebutton.Name = "Dispensebutton";
+            this.Dispensebutton.Size = new System.Drawing.Size(105, 43);
+            this.Dispensebutton.TabIndex = 0;
+            this.Dispensebutton.Text = "Dispense";
+            this.Dispensebutton.UseVisualStyleBackColor = true;
+            this.Dispensebutton.Click += new System.EventHandler(this.Dispensebutton_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clearbutton.Location = new System.Drawing.Point(164, 434);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(105, 43);
+            this.Clearbutton.TabIndex = 3;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exitbutton.Location = new System.Drawing.Point(299, 434);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(105, 43);
+            this.Exitbutton.TabIndex = 4;
+            this.Exitbutton.Text = "Exit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.HoneycheckBox);
+            this.groupBox3.Controls.Add(this.SugarcheckBox);
+            this.groupBox3.Controls.Add(this.CreamcheckBox);
+            this.groupBox3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(223, 268);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 142);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Extras";
+            // 
+            // CreamcheckBox
+            // 
+            this.CreamcheckBox.AutoSize = true;
+            this.CreamcheckBox.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreamcheckBox.Location = new System.Drawing.Point(25, 27);
+            this.CreamcheckBox.Name = "CreamcheckBox";
+            this.CreamcheckBox.Size = new System.Drawing.Size(82, 22);
+            this.CreamcheckBox.TabIndex = 5;
+            this.CreamcheckBox.Text = "Cream";
+            this.CreamcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SugarcheckBox
+            // 
+            this.SugarcheckBox.AutoSize = true;
+            this.SugarcheckBox.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SugarcheckBox.Location = new System.Drawing.Point(25, 65);
+            this.SugarcheckBox.Name = "SugarcheckBox";
+            this.SugarcheckBox.Size = new System.Drawing.Size(76, 22);
+            this.SugarcheckBox.TabIndex = 6;
+            this.SugarcheckBox.Text = "Sugar";
+            this.SugarcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // HoneycheckBox
+            // 
+            this.HoneycheckBox.AutoSize = true;
+            this.HoneycheckBox.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HoneycheckBox.Location = new System.Drawing.Point(25, 105);
+            this.HoneycheckBox.Name = "HoneycheckBox";
+            this.HoneycheckBox.Size = new System.Drawing.Size(78, 22);
+            this.HoneycheckBox.TabIndex = 7;
+            this.HoneycheckBox.Text = "Honey";
+            this.HoneycheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PepsiradioButton
+            // 
+            this.PepsiradioButton.AutoSize = true;
+            this.PepsiradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PepsiradioButton.Location = new System.Drawing.Point(43, 27);
+            this.PepsiradioButton.Name = "PepsiradioButton";
+            this.PepsiradioButton.Size = new System.Drawing.Size(74, 22);
+            this.PepsiradioButton.TabIndex = 5;
+            this.PepsiradioButton.TabStop = true;
+            this.PepsiradioButton.Text = "Pepsi";
+            this.PepsiradioButton.UseVisualStyleBackColor = true;
+            // 
+            // DietradioButton
+            // 
+            this.DietradioButton.AutoSize = true;
+            this.DietradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DietradioButton.Location = new System.Drawing.Point(43, 65);
+            this.DietradioButton.Name = "DietradioButton";
+            this.DietradioButton.Size = new System.Drawing.Size(107, 22);
+            this.DietradioButton.TabIndex = 6;
+            this.DietradioButton.TabStop = true;
+            this.DietradioButton.Text = "Diet Pepsi";
+            this.DietradioButton.UseVisualStyleBackColor = true;
+            // 
+            // SMradioButton
+            // 
+            this.SMradioButton.AutoSize = true;
+            this.SMradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SMradioButton.Location = new System.Drawing.Point(239, 65);
+            this.SMradioButton.Name = "SMradioButton";
+            this.SMradioButton.Size = new System.Drawing.Size(109, 22);
+            this.SMradioButton.TabIndex = 7;
+            this.SMradioButton.TabStop = true;
+            this.SMradioButton.Text = "Sierra Mist";
+            this.SMradioButton.UseVisualStyleBackColor = true;
+            // 
+            // RTearadioButton
+            // 
+            this.RTearadioButton.AutoSize = true;
+            this.RTearadioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTearadioButton.Location = new System.Drawing.Point(43, 107);
+            this.RTearadioButton.Name = "RTearadioButton";
+            this.RTearadioButton.Size = new System.Drawing.Size(174, 22);
+            this.RTearadioButton.TabIndex = 8;
+            this.RTearadioButton.TabStop = true;
+            this.RTearadioButton.Text = "Brisk Raspberry Tea";
+            this.RTearadioButton.UseVisualStyleBackColor = true;
+            // 
+            // PTearadioButton
+            // 
+            this.PTearadioButton.AutoSize = true;
+            this.PTearadioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PTearadioButton.Location = new System.Drawing.Point(239, 27);
+            this.PTearadioButton.Name = "PTearadioButton";
+            this.PTearadioButton.Size = new System.Drawing.Size(147, 22);
+            this.PTearadioButton.TabIndex = 9;
+            this.PTearadioButton.TabStop = true;
+            this.PTearadioButton.Text = "Brisk Peach Tea";
+            this.PTearadioButton.UseVisualStyleBackColor = true;
+            // 
+            // WaterradioButton
+            // 
+            this.WaterradioButton.AutoSize = true;
+            this.WaterradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WaterradioButton.Location = new System.Drawing.Point(239, 107);
+            this.WaterradioButton.Name = "WaterradioButton";
+            this.WaterradioButton.Size = new System.Drawing.Size(95, 22);
+            this.WaterradioButton.TabIndex = 10;
+            this.WaterradioButton.TabStop = true;
+            this.WaterradioButton.Text = "Aquafina";
+            this.WaterradioButton.UseVisualStyleBackColor = true;
+            // 
+            // CoffeeradioButton
+            // 
+            this.CoffeeradioButton.AutoSize = true;
+            this.CoffeeradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CoffeeradioButton.Location = new System.Drawing.Point(20, 27);
+            this.CoffeeradioButton.Name = "CoffeeradioButton";
+            this.CoffeeradioButton.Size = new System.Drawing.Size(80, 22);
+            this.CoffeeradioButton.TabIndex = 6;
+            this.CoffeeradioButton.TabStop = true;
+            this.CoffeeradioButton.Text = "Coffee";
+            this.CoffeeradioButton.UseVisualStyleBackColor = true;
+            // 
+            // TearadioButton
+            // 
+            this.TearadioButton.AutoSize = true;
+            this.TearadioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TearadioButton.Location = new System.Drawing.Point(20, 64);
+            this.TearadioButton.Name = "TearadioButton";
+            this.TearadioButton.Size = new System.Drawing.Size(58, 22);
+            this.TearadioButton.TabIndex = 7;
+            this.TearadioButton.TabStop = true;
+            this.TearadioButton.Text = "Tea";
+            this.TearadioButton.UseVisualStyleBackColor = true;
+            // 
+            // HCradioButton
+            // 
+            this.HCradioButton.AutoSize = true;
+            this.HCradioButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HCradioButton.Location = new System.Drawing.Point(20, 104);
+            this.HCradioButton.Name = "HCradioButton";
+            this.HCradioButton.Size = new System.Drawing.Size(132, 22);
+            this.HCradioButton.TabIndex = 8;
+            this.HCradioButton.TabStop = true;
+            this.HCradioButton.Text = "Hot Chocolate";
+            this.HCradioButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 489);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.Dispensebutton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Drink Dispenser";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Dispensebutton;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Button Exitbutton;
+        private System.Windows.Forms.RadioButton WaterradioButton;
+        private System.Windows.Forms.RadioButton PTearadioButton;
+        private System.Windows.Forms.RadioButton RTearadioButton;
+        private System.Windows.Forms.RadioButton SMradioButton;
+        private System.Windows.Forms.RadioButton DietradioButton;
+        private System.Windows.Forms.RadioButton PepsiradioButton;
+        private System.Windows.Forms.RadioButton HCradioButton;
+        private System.Windows.Forms.RadioButton TearadioButton;
+        private System.Windows.Forms.RadioButton CoffeeradioButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox HoneycheckBox;
+        private System.Windows.Forms.CheckBox SugarcheckBox;
+        private System.Windows.Forms.CheckBox CreamcheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿// Zailyn Tamayo
+// CSC 670 User Interface Engineering
+// Extra Credit Assignment
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drink_Dispenser_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Dispense button clicked
+        private void Dispensebutton_Click(object sender, EventArgs e)
+        {
+            // User can only select a cold drink or a hot drink
+            if (PepsiradioButton.Checked == true | DietradioButton.Checked == true |
+                RTearadioButton.Checked == true | PTearadioButton.Checked == true |
+                SMradioButton.Checked == true | WaterradioButton.Checked == true &&
+                CoffeeradioButton.Checked == true | TearadioButton.Checked == true |
+                HCradioButton.Checked == true | CreamcheckBox.Checked == true |
+                SugarcheckBox.Checked == true | HoneycheckBox.Checked == true)
+            {
+                // Message to clear selection appears if user chooses both cold and hot
+                MessageBox.Show("Please clear selection before choosing a Drink!");
+            }
+                // Cold drinks selection
+            else if (PepsiradioButton.Checked == true)
+                MessageBox.Show("Pepsi dispensed!");
+            else if (DietradioButton.Checked == true)
+                MessageBox.Show("Diet Pepsi dispensed!");
+            else if (RTearadioButton.Checked == true)
+                MessageBox.Show("Brisk Raspberry Tea dispensed!");
+            else if (PTearadioButton.Checked == true)
+                MessageBox.Show("Brisk Peach Tea dispensed!");
+            else if (SMradioButton.Checked == true)
+                MessageBox.Show("Sierra Mist dispensed!");
+            else if (WaterradioButton.Checked == true)
+                MessageBox.Show("AquaFina dispensed!");
+
+                // Hot drinks selection
+            else if (HCradioButton.Checked == true)
+                MessageBox.Show("Hot Chocolate dispensed!");
+            else if (TearadioButton.Checked == true)
+            {
+                // Extra options for tea
+                if (CreamcheckBox.Checked == true && SugarcheckBox.Checked == false && HoneycheckBox.Checked == false)
+                    MessageBox.Show("Tea with Cream dispensed!");
+                else if (CreamcheckBox.Checked == true && SugarcheckBox.Checked == true)
+                    MessageBox.Show("Tea with Cream and Sugar dispensed!");
+                else if (CreamcheckBox.Checked == true && HoneycheckBox.Checked == true)
+                    MessageBox.Show("Tea with Cream and Honey dispensed!");
+                else if (SugarcheckBox.Checked == true && CreamcheckBox.Checked == false && HoneycheckBox.Checked == false)
+                    MessageBox.Show("Tea with Sugar dispensed!");
+                else if (HoneycheckBox.Checked == true && CreamcheckBox.Checked == false && SugarcheckBox.Checked == false)
+                    MessageBox.Show("Tea with Honey dispensed!");
+                else
+                    MessageBox.Show("Hot Tea dispensed!");
+            }
+            else if (CoffeeradioButton.Checked == true)
+            {
+                //Extra options for coffee
+                if (CreamcheckBox.Checked == true && SugarcheckBox.Checked == false && HoneycheckBox.Checked == false)
+                    MessageBox.Show("Coffee with Cream dispensed!");
+                else if (CreamcheckBox.Checked == true && SugarcheckBox.Checked == true)
+                    MessageBox.Show("Coffee with Cream and Sugar dispensed!");
+                else if (CreamcheckBox.Checked == true && HoneycheckBox.Checked == true)
+                    MessageBox.Show("Coffee with Cream and Honey dispensed!");
+                else if (SugarcheckBox.Checked == true && CreamcheckBox.Checked == false && HoneycheckBox.Checked == false)
+                    MessageBox.Show("Coffee with Sugar dispensed!");
+                else if (HoneycheckBox.Checked == true && CreamcheckBox.Checked == false && SugarcheckBox.Checked == false)
+                    MessageBox.Show("Coffee with Honey dispensed!");
+                else
+                    MessageBox.Show("Black Coffee dispensed!");
+            }
+           
+        }
+
+        // Clear button clicked
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            // Clears all selections
+            PepsiradioButton.Checked = false;
+            DietradioButton.Checked = false;
+            RTearadioButton.Checked = false;
+            PTearadioButton.Checked = false;
+            SMradioButton.Checked = false;
+            WaterradioButton.Checked = false;
+            CoffeeradioButton.Checked = false;
+            TearadioButton.Checked = false;
+            HCradioButton.Checked = false;
+            CreamcheckBox.Checked = false;
+            SugarcheckBox.Checked = false;
+            HoneycheckBox.Checked = false;
+        }
+
+        // Exit button clicked
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            // Exits program
+            this.Close();
+        }
+    }
+}
